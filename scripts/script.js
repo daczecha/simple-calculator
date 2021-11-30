16703c90dd1d5187bfeb64890b50409e6c425f2e
daczecha <daczecha@gmail.com>
2021-11-30 17:38:33 +0400

    Fix bugs

--- scripts/script.js
+++ scripts/script.js
@@ -1,132 +1,157 @@
-const numberButtons = document.querySelectorAll("[data-number]");
-const operatorButtons = document.querySelectorAll("[data-operator]");
-const equalsButton = document.querySelector("#equalsBtn");
-const deleteButton = document.querySelector("#deleteBtn");
-const clearButton = document.querySelector("#clearBtn");
-const lastOperationScreen = document.querySelector("#lastOperationScreen");
-const currentOperationScreen = document.querySelector("#currentOperationScreen");
-
-
-
-let currentOperand = '';
-let lastOperand = '';
-let operation = undefined;
-
-function AppendNumber(num){
-    if(num === '.' && currentOperand.includes('.')) return;
-    currentOperand = currentOperand.toString() + num.toString();
-}
-
-function AppendOperator(op){
-    console.log(op);
-    operation = op;
-    if(currentOperand === '') return;
-    if(lastOperand !== '') {Compute();}
-    lastOperand = currentOperand + operation.toString();
-    currentOperand = '';
-}
-
-
-function Delete(){
-    currentOperand = currentOperand.toString().slice(0, -1);
-}
-
-function Clear() {
-    currentOperand = '';
-    lastOperand = '';
-    operation = undefined;
-}
-
-function UpdateDisplay(screen, operand){
-    screen.value = operand;
-}
-
-function Compute(){
-    let result;
-    if(currentOperand === "") currentOperand = '0';
-    const last = parseFloat(lastOperand);
-    const current = parseFloat(currentOperand);
-    if(isNaN(current) && isNaN(last)) return;
-    switch(operation){
-        case "+":
-            result = last + current;
-            break;
-        case "-":
-            result = last - current;
-            break;
-        case "×":
-            result = last * current;
-            break;
-        case "÷":
-            result = last / current;
-            break;
-        default:
-            return
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+      this.previousOperandTextElement = previousOperandTextElement
+      this.currentOperandTextElement = currentOperandTextElement
+      this.clear()
     }
-    UpdateDisplay(lastOperationScreen, `${lastOperand}${currentOperand}=`);
-    currentOperand  = RoundResult(result);
-    operation = undefined;
-    lastOperand = '';
-}
+  
+    clear() {
+      this.currentOperand = '0'
+      this.previousOperand = '0'
+      this.operation = undefined
+    }
+  
+    delete() {
+        if(this.currentOperand !=='0')
+            this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    }
+  
+    appendNumber(number) {
+      if (number === '.' && this.currentOperand.includes('.')) return
+      this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+  
+    chooseOperation(operation) {
+      if (this.currentOperand === '') return
+      if (this.previousOperand !== '') {
+        this.compute()
+      }
+      this.operation = operation
+      this.previousOperand = this.currentOperand
+      this.currentOperand = '0'
+    }
+  
+    compute() {
+      let computation
+      const prev = parseFloat(this.previousOperand)
+      const current = parseFloat(this.currentOperand)
+      if (isNaN(prev) || isNaN(current)) return
+      switch (this.operation) {
+        case '+':
+          computation = prev + current
+          break
+        case '-':
+          computation = prev - current
+          break
+        case '*':
+          computation = prev * current
+          break
+        case '÷':
+          computation = prev / current
+          break
+        default:
+          return
+      }
+      this.currentOperand = computation
+      this.operation = undefined
+      this.previousOperand = ''
+    }
+  
+    getDisplayNumber(number) {
+      const stringNumber = number.toString()
+      const integerDigits = parseFloat(stringNumber.split('.')[0])
+      const decimalDigits = stringNumber.split('.')[1]
+      let integerDisplay
+      if (isNaN(integerDigits)) {
+        integerDisplay = ''
+      } else {
+        integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+      }
+      if (decimalDigits != null) {
+        return `${integerDisplay}.${decimalDigits}`
+      } else {
+        return integerDisplay
+      }
+    }
+  
+    updateDisplay() {
+      this.currentOperandTextElement.placeholder =
+        this.getDisplayNumber(this.currentOperand)
+      if (this.operation != null) {
+        this.previousOperandTextElement.placeholder =
+          `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+      } else {
+        this.previousOperandTextElement.placeholder = '0'
+      }
+    }
+  }
+  
+  
+  const numberButtons = document.querySelectorAll('[data-number]')
+  const operationButtons = document.querySelectorAll('[data-operation]')
+  const equalsButton = document.querySelector('[data-equals]')
+  const deleteButton = document.querySelector('[data-delete]')
+  const allClearButton = document.querySelector('[data-all-clear]')
+  const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+  const currentOperandTextElement = document.querySelector('[data-current-operand]')
+  
+  const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+  
+  window.addEventListener('keydown', handleKeyboardInput);
+
+  numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.appendNumber(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
+  
+  operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.chooseOperation(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
+  
+  equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+  })
+  
+  allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+  })
+  
+  deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+  })
 
 
-window.addEventListener('keydown', handleKeyboardInput);
 
-function RoundResult(num){
-    return Math.round(num * 100) / 100;
-}
 
-numberButtons.forEach(button => {
-    button.addEventListener("click",() => {
-        AppendNumber(button.innerText);
-        UpdateDisplay(currentOperationScreen, currentOperand);
-    });
-});
-
-operatorButtons.forEach(button => {
-    button.addEventListener("click",() => {
-        AppendOperator(button.id);
-        UpdateDisplay(lastOperationScreen, lastOperand);
-    });
-});
-
-equalsButton.addEventListener('click', button => {
-    Compute();
-    UpdateDisplay(currentOperationScreen, currentOperand);
-});
-
-clearButton.addEventListener('click', button => {
-    Clear();
-    UpdateDisplay(lastOperationScreen, lastOperand);
-    UpdateDisplay(currentOperationScreen,currentOperand);
-});
-
-deleteButton.addEventListener('click', button => {
-    Delete();
-    UpdateDisplay(currentOperationScreen,currentOperand);
-});
 
 function handleKeyboardInput(e){
     if (e.key >= 0 && e.key <= 9 || e.key === '.') {
-        AppendNumber(e.key);
-        UpdateDisplay(currentOperationScreen, currentOperand);
+        calculator.appendNumber(e.key)
+        calculator.updateDisplay()
     }
     if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/'){
-        AppendOperator(convertOperator(e.key));
-        UpdateDisplay(lastOperationScreen,lastOperand);
+        calculator.chooseOperation(convertOperator(e.key))
+        calculator.updateDisplay()
     }
     if (e.key === '=' || e.key === 'Enter'){
-        Compute();
-        UpdateDisplay(currentOperationScreen, currentOperand);
+        calculator.compute()
+        calculator.updateDisplay()
     }
     if (e.key === 'Backspace') {
-        Delete();
-        UpdateDisplay(currentOperationScreen,currentOperand);
+        calculator.delete()
+        calculator.updateDisplay()
     };
     if (e.key === 'Escape' || e.key === "Backspace" && e.ctrlKey) {
-        Clear();
-        UpdateDisplay(lastOperationScreen, lastOperand);
-        UpdateDisplay(currentOperationScreen,currentOperand);
+        calculator.clear()
+        calculator.updateDisplay()
     };
 }
 function convertOperator(keyboardOperator) {
@@ -138,33 +163,36 @@ function convertOperator(keyboardOperator) {
 
 
 
-function Theme(str){
-    const buttons = document.querySelectorAll("button");
-    const wrapper = document.querySelector(".wrapper");
-    const input = document.querySelector(".input");
-    const lastOperationScreen = document.querySelector("#lastOperationScreen");
-    const currentOperationScreen = document.querySelector("#currentOperationScreen");
-    const body = document.querySelector("body");
 
-    const light = document.querySelector('#light');
-    const dark = document.querySelector('#dark');
+
+
+function Theme(str){
+    const buttons = document.querySelectorAll("button")
+    const wrapper = document.querySelector(".wrapper")
+    const input = document.querySelector(".input")
+    const lastOperationScreen = document.querySelector("#lastOperationScreen");
+    const currentOperationScreen = document.querySelector("#currentOperationScreen")
+    const body = document.querySelector("body")
+
+    const light = document.querySelector('#light')
+    const dark = document.querySelector('#dark')
 
     if(str === "light"){
-        body.style.background =`linear-gradient(90deg, rgba(220,220,220,1) 0%, rgba(240,240,240,1) 100%)`;
-        light.style.opacity = 1;
-        dark.style.opacity = 0.3;
+        body.style.background =`linear-gradient(90deg, rgba(220,220,220,1) 0%, rgba(240,240,240,1) 100%)`
+        light.style.opacity = 1
+        dark.style.opacity = 0.3
     }else{
-        body.style.background = `linear-gradient(90deg, rgba(34,37,45,1) 0%, rgba(39,43,51,1) 100%)`;
-        dark.style.opacity = 1;
-        light.style.opacity = 0.3;
+        body.style.background = `linear-gradient(90deg, rgba(34,37,45,1) 0%, rgba(39,43,51,1) 100%)`
+        dark.style.opacity = 1
+        light.style.opacity = 0.3
     }
 
-    lastOperationScreen.style.color = `var(--${str}-text)`;
-    currentOperationScreen.style.color = `var(--${str}-text)`;
-    wrapper.style.backgroundColor = `var(--${str}-wrapper)`;
-    input.style.backgroundColor = `var(--${str}-input)`;
+    lastOperationScreen.style.color = `var(--${str}-text)`
+    currentOperationScreen.style.color = `var(--${str}-text)`
+    wrapper.style.backgroundColor = `var(--${str}-wrapper)`
+    input.style.backgroundColor = `var(--${str}-input)`
     buttons.forEach(button =>{
-        button.style.backgroundColor = `var(--${str}-button)`;
-        button.style.color = `var(--${str}-text)`;
+        button.style.backgroundColor = `var(--${str}-button)`
+        button.style.color = `var(--${str}-text)`
     })
 }
